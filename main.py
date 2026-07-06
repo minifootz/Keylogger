01601915199d9e169f393e0107d99a8c9c3445e6
JACK DONLEVY <2311359@chester.ac.uk>
2026-07-06 21:53:45 +0100

    fixed error in read inputs not being uploaded

--- main.py
+++
@@ -1,9 +0,0 @@
-from pynput import keyboard 
-
-def on_press(key):
-    with keyboard.Listener(on_press=on_press) as listener:
-        with open(".gitignore" , 'w') as f: # writes to git.ignore file
-            f.write("key logging started\n")
-            f.write(f" key {key} pressed\n")
-        listener.join()
-
